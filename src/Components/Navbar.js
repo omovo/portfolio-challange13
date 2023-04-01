a1621f45d4ca1a204ec5708b3414d3fa801c1c13
TENNISON-OMOVOH <omovohtennison@gmail.com>
2023-04-01 19:58:30 +0100

    Navbar Completed

---
+++ src/Components/Navbar.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+  <a className="navbar-brand" href="#">DIGITAL ZONE</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+    <ul className="navbar-nav ml-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">About</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Portfolio</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Contact</a>
+      </li>
+    
+    </ul>
+    
+  </div>
+
+</nav>
+    </div>
+  )
+}
+
+export default Navbar
